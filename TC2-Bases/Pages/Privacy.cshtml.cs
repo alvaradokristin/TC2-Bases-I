d2fccf1aa02163c170a3ae838913bd3812171b88
Kristin Alvarado G <alvaradokristin@gmail.com>
2022-10-05 18:38:24 -0600

    Visual Studio Project Created

---
+++ TC2-Bases/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TC2_Bases.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
